207f2dc589a87847c080f92c2eb89ce03955af2b
Christophe Banga <christophebanga@gmail.com>
2018-06-06 10:48:45 +0300

    Add function to visualize location coordinates and another function to send them via email

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,23 @@ $("button#location").click(function(event){
   $("#header").hide();
   $("input#map").show();
 })
+
 //$("form#subscription").submit(function(){
   //event.preventDefault();
   //alert("Congratulations! You have successfully registered.")
 //})
   function sendMail(){
-    var link=   "mailto:"+document.getElementById("sponsors").value;
-                +"?cc="
-                //+"&body=" + escape(document.getElementById("send").value)
-            ;
-
+    var email = document.getElementById('mailTo').value
+    var subject = "This is my location"
+    var body = document.getElementById('subjectText').value
+    var link= "mailto:"+ email
+            + "?subject=" + subject
+            + "&body=" + body
+    ;
             window.location.href = link;
   }
 
-  var map, infoWindow;
+  var map, infoWindow, myLink;
   function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
       //adjust coordinate so that map starts in Nairobi on reload
@@ -36,8 +39,10 @@ $("button#location").click(function(event){
         };
 
         var myLink = "https://www.google.com/maps/@"+position.coords.latitude+","+position.coords.longitude+",16z";
-        console.log (myLink);
-        alert("Your coordinates are" + "," + myLink);
+        $("button#toLink").click(function(event){
+          event.preventDefault();
+          $("#showLink").append(myLink);
+        })
 
         infoWindow.setPosition(pos);
         infoWindow.setContent('Location found.');
